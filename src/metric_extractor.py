bbb8d291152b2fd1d0ae308fca1e604e44d5357b
Carlos Aumente <carlosaumente@gmail.com>
2024-09-03 12:20:44 +0200

    Added format standards

--- src/metric_extractor.py
+++ src/metric_extractor.py
@@ -1,52 +1,29 @@
-import pandas as pd
 from pathlib import Path
-from src.utils.sequences import get_spacing
-from src.utils.operations.file_operations import ls_dirs, load_config_file
-from src.utils.operations.misc_operations import fancy_print, fancy_tqdm
-from src.utils.sequences import read_segmentation, read_prediction
-from src.metrics.custom_metrics import calculate_metrics, one_hot_encoding
-from src.utils.sequences import load_subject_nii
+
+import pandas as pd
 from colorama import Fore
 
-# TODO: check if removing this two lines
-# custom params for pycharm
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1000)
+from src.metrics.custom_metrics import calculate_metrics, one_hot_encoding
+from src.utils.operations.file_operations import ls_dirs, load_config_file
+from src.utils.operations.misc_operations import fancy_print, fancy_tqdm
+from src.utils.sequences import get_spacing
+from src.utils.sequences import load_subject_nii
+from src.utils.sequences import read_segmentation, read_prediction
 
 
-def process_metric(data, models):
-    metrics_cols = data.drop(columns=["ID", "region", "model"]).columns
-    post_processed_metrics = []
-
-    for metric in metrics_cols:
-        df_ = data[['ID', 'region', 'model', metric]]
-        pivot_df = df_.pivot_table(index=['ID', 'region'], columns='model', values=metric).reset_index()
-
-        # difference of all models with respect to the baseline model
-        for model in models:
-            if model != 'baseline':
-                pivot_df[f'{model}'] = 100 * (pivot_df[f'{model}'] - pivot_df[f'baseline']) / pivot_df[f'baseline']
-
-        pivot_df.drop(columns='baseline', inplace=True)
-        pivot_df['metric'] = metric
-        post_processed_metrics.append(pivot_df)
-
-    out = pd.concat(post_processed_metrics)
-    out = pd.melt(out, id_vars=['ID', 'region', 'metric'], value_vars=['model_1', 'model_2'])
-
-    return out
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # config variables
     config = load_config_file("./src/configs/metric_extractor.yml")
-    label_names, numeric_label = list(config["labels"].keys()), list(config["labels"].values())
-    output_path = config['output_path']
+    label_names, numeric_label = (
+        list(config["labels"].keys()),
+        list(config["labels"].values()),
+    )
+    output_path = config["output_path"]
     Path(output_path).mkdir(parents=True, exist_ok=True)
 
     # load paths to test data
-    path_test_dataset = config['ground_truth_data_path']
+    path_test_dataset = config["ground_truth_data_path"]
     patients_list = ls_dirs(path_test_dataset)
 
     """
@@ -58,7 +35,7 @@ if __name__ == '__main__':
     # load paths to predictions
     models = config["model_predictions_paths"]
     for model_name, path_predictions in models.items():
-        fancy_print(f"\nStarting metric extraction for model {model_name}", Fore.LIGHTMAGENTA_EX, '✨')
+        fancy_print(f"\nStarting metric extraction for model {model_name}", Fore.LIGHTMAGENTA_EX, "✨")
 
         # loop over all the elements in the root folder
         with fancy_tqdm(total=len(patients_list), desc=f"{Fore.CYAN}Progress", leave=True) as pbar:
@@ -66,10 +43,7 @@ if __name__ == '__main__':
                 pbar.set_postfix_str(f"{Fore.CYAN}Current patient: {Fore.LIGHTBLUE_EX}{ID}{Fore.CYAN}")
                 pbar.update(1)
                 if n % 10 == 0 and n > 0:
-                    fancy_print(f"Processed {n} patients", Fore.CYAN, '🔹')
-
-                # if n > 5:
-                #     break
+                    fancy_print(f"Processed {n} patients", Fore.CYAN, "🔹")
 
                 # read ground truth segmentation and prediction
                 gt = read_segmentation(root=path_test_dataset, patient_id=ID)
@@ -81,7 +55,9 @@ if __name__ == '__main__':
                 pred = one_hot_encoding(pred, numeric_label)
 
                 # compute metrics
-                metrics = calculate_metrics(ground_truth=gt, segmentation=pred, patient=ID, regions=label_names, spacing=spacing)
+                metrics = calculate_metrics(
+                    ground_truth=gt, segmentation=pred, patient=ID, regions=label_names, spacing=spacing,
+                )
 
                 # from list of dict to dataframe
                 patient_info_df = pd.DataFrame(metrics)
@@ -93,4 +69,4 @@ if __name__ == '__main__':
                 raw_metrics = pd.concat([raw_metrics, patient_info_df], ignore_index=True)
 
     # saving raw results
-    raw_metrics.to_csv(f"{output_path}/{config['filename']}.csv", index=False)
+    raw_metrics.to_csv(f"{output_path}/extracted_information_{config['filename']}.csv", index=False)
