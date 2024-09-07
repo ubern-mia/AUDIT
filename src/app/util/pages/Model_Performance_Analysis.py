f634ffd93acf7872d8e7c87d347a924487d1ed1d
Carlos Aumente <carlosaumente@gmail.com>
2024-09-07 23:33:55 +0200

    Model performance adapted to the new config file

--- src/app/util/pages/Model_Performance_Analysis.py
+++ src/app/util/pages/Model_Performance_Analysis.py
@@ -189,7 +189,7 @@ def performance():
     """
 
     # Load configuration file
-    config = load_config_file("./src/configs/app.yml").get("model_performance_analysis", {})
+    config = load_config_file("./src/configs/app_test.yml")
     if not config:
         st.error("Please set the configuration file up with a 'model_performance_analysis' key.")
         sys.exit()
